8b6e271bae86c1a7294cf337414c2fd77e7335cc
Sayemm <sayemseer@gmail.com>
2025-10-20 19:14:00 -0600

    retry download attempt

--- python-downloader/download.py
+++ python-downloader/download.py
@@ -1,6 +1,7 @@
 import os
 import kagglehub
 import shutil
+import time
 
 username = os.getenv("KAGGLE_USERNAME")
 api_key = os.getenv("KAGGLE_KEY")
@@ -10,9 +11,24 @@ if not username or not api_key:
 print(f"logged in as: {username}")
 
 try:
-    path = kagglehub.dataset_download("arshkon/linkedin-job-postings")
+    print("Downloading data....")
+    max_retries = 3
+    for attempt in range(max_retries):
+        try:
+            os.environ['KAGGLE_TIMEOUT'] = '300'
+            path = kagglehub.dataset_download("arshkon/linkedin-job-postings")
+            break
+        except Exception as download_error:
+            if attempt < max_retries - 1:
+                wait_time = (attempt + 1) * 30
+                time.sleep(wait_time)
+                print(f"Download Failed: {download_error}")
+                print(f"Waiting {wait_time} before retry")
+            else:
+                raise
+    
     print(f"Downloaded to:{path}")
-
+    
     csv_file = os.path.join(path, "postings.csv")
     if not os.path.exists(csv_file):
         print("posting.csv not found!")
@@ -22,9 +38,9 @@ try:
     shutil.copy(csv_file, "/data/postings.csv")
 
     size_bytes = os.path.getsize("/data/postings.csv")
-    size_mb = size_bytes (1024*1024)
+    size_mb = size_bytes / (1024*1024)
     print(f"File Ready: {size_mb} MB")
 
 except Exception as e:
-    print(f"Error: {r}")
+    print(f"Error: {e}")
     exit(1)
