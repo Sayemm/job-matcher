9061d06fa8a46148028d970d4cf03d2ea17defbb
Sayemm <sayemseer@gmail.com>
2025-10-22 12:50:21 -0600

    go-loader: database connection

---
+++ go-loader/internal/infrastructure/database/postgres.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/Sayemm/job-matcher/go-loader/config"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func GetConnectionString(cnf *config.DBConfig) string {
+	connString := fmt.Sprintf(
+		"user=%s password=%s host=%s port=%d dbname=%s",
+		cnf.User, cnf.Password, cnf.Host, cnf.Port, cnf.Name,
+	)
+	if !cnf.EnableSSLMode {
+		connString += " sslmode=disable"
+	}
+	return connString
+}
+
+func NewConnection(cnf *config.DBConfig) (*sqlx.DB, error) {
+	dbSource := GetConnectionString(cnf)
+	dbCon, err := sqlx.Connect("postgres", dbSource)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return dbCon, nil
+}
