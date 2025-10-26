848446ac40bb33abeaff4e52cb1ad04c6220639d
Sayemm <sayemseer@gmail.com>
2025-10-26 00:24:07 -0600

    DDD: move domain file

---
+++ go-loader/internal/domain/job.go
@@ -0,0 +1,21 @@
+package domain
+
+import "time"
+
+type Job struct {
+	ID    int    `db:"id" json:"id"`
+	JobID string `db:"job_id" json:"job_id"`
+
+	CompanyName string `db:"company_name" json:"company_name"`
+	Title       string `db:"title" json:"title"`
+	Description string `db:"description" json:"description"`
+	Location    string `db:"location" json:"location"`
+
+	RemoteAllowed   bool     `db:"remote_allowed" json:"remote_allowed"`
+	ExperienceLevel string   `db:"experience_level" json:"experience_level"`
+	MinSalary       *float64 `db:"min_salary" json:"min_salary,omitempty"`
+	MaxSalary       *float64 `db:"max_salary" json:"max_salary,omitempty"`
+
+	ClusterID *int      `db:"cluster_id" json:"cluster_id,omitempty"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+}
--- go-loader/internal/domain/repositories.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import "github.com/Sayemm/job-matcher/go-loader/internal/domain/entities"
-
-type JobRepository interface {
-	SaveBath(jobs []*entities.Job) error
-	Count() (int, error)
-}
