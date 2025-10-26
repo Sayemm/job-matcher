314737b8ee9710335bfcebb88aa0b94110c78ae8
Sayemm <sayemseer@gmail.com>
2025-10-26 00:27:34 -0600

    DDD: Wiring together on main

--- go-loader/cmd/serve.go
+++ go-loader/cmd/serve.go
@@ -5,7 +5,10 @@ import (
 	"os"
 
 	"github.com/Sayemm/job-matcher/go-loader/config"
+	"github.com/Sayemm/job-matcher/go-loader/internal/infrastructure/csv"
 	"github.com/Sayemm/job-matcher/go-loader/internal/infrastructure/database"
+	"github.com/Sayemm/job-matcher/go-loader/internal/infrastructure/repository"
+	"github.com/Sayemm/job-matcher/go-loader/internal/service"
 )
 
 func Serve() {
@@ -17,10 +20,23 @@ func Serve() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer dbCon.Close()
 
 	err = database.MigrateDB(dbCon, "./migrations")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	// REPO
+	jobRepo := repository.NewJobRepository(dbCon)
+	csvReader := csv.NewReader(cnf.CSVPath)
+
+	// SERVICE
+	service := service.NewJobLoaderService(
+		jobRepo,
+		csvReader,
+		cnf.BatchSize,
+	)
+	service.LoadJobs()
 }
