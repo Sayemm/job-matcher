c06e7916adad95ca3f6914ff5c437ce1a93c73af
Sayemm <sayemseer@gmail.com>
2025-10-30 17:04:30 -0600

    go-api: api for fetching job data -> config

---
+++ go-api/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import "os"
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBName     string
+	DBUser     string
+	DBPassword string
+	ServerPort string
+}
+
+var configurations *Config
+
+func loadConfig() *Config {
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBName:     getEnv("DB_NAME", "jobmatcher"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func GetConfig() *Config {
+	if configurations == nil {
+		configurations = loadConfig()
+	}
+	return configurations
+}
